f890a46fa381163487611d50b1090e8731cd775b
Kesit Budi Kusumo <kesit@kernel.id>
2024-05-29 22:15:54 +0700

    feat: add bottom-up recursive solution

--- main.go
+++ main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	input := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 9,
+		},
+		Right: &TreeNode{
+			Val: 20,
+			Left: &TreeNode{
+				Val: 15,
+			},
+			Right: &TreeNode{
+				Val: 7,
+			},
+		},
+	}
+	output := maxDepthFromBottomUp(input)
+	fmt.Println(output)
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepthFromBottomUp(root *TreeNode) int {
+	var recursiveFn func(*TreeNode, int) int
+	recursiveFn = func(node *TreeNode, depth int) int {
+		if node == nil {
+			return depth - 1
+		}
+		leftDepth := recursiveFn(node.Left, depth+1)
+		rightDepth := recursiveFn(node.Right, depth+1)
+		return max(leftDepth, rightDepth)
+	}
+
+	return recursiveFn(root, 1)
 }
